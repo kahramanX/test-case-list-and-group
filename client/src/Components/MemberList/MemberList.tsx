f055a8b92fc644b958e6b48896e3d53beb6f6dc7
Kahraman <80291704+kahramanX@users.noreply.github.com>
2022-11-24 03:14:01 +0300

    added button and badge components

--- client/src/Components/MemberList/MemberList.tsx
+++ client/src/Components/MemberList/MemberList.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 import Member from "./Member";
 import "Assets/Styles/membersList.scss";
+import Badge from "Components/Shared/Badge";
+import Button from "Components/Shared/Button";
 
 type Props = {};
 
 const MemberList: React.FC = (props: Props) => {
-  function openAddMemberModal(): void {
-    console.log("open add member modal");
+  function openAddMemberModal(): any {
+    return console.log("open add member modal");
   }
 
   return (
     <div className="members-list-container">
       <div className="members-list-title-row">
         <div className="members-list-title">Member List</div>
-        <div className="members-count">31</div>
+        <Badge text={"31"} color={"blue"} exClass={"member-count"} />
       </div>
       <div className="members-list">
         {[0, 1, 2, 3, 4, 5, 6, 7].map((index) => (
           <Member key={index} />
         ))}
       </div>
-      <button className="add-members-btn" onClick={openAddMemberModal}>
-        <span className="material-symbols-rounded">add</span>
-        Add Member
-      </button>
+      <Button
+        iconName={"add"}
+        text={"Add Member"}
+        color={"blue"}
+        action={(): any => openAddMemberModal}
+      />
     </div>
   );
 };
---
+++ client/src/Components/Shared/Button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "Assets/Styles/componentsStyle/button.scss";
+
+type Props = {
+  text: string;
+  iconName?: string;
+  exClass?: string;
+  color: string;
+  action?: any;
+};
+
+const Button: React.FC<Props> = ({
+  text,
+  iconName,
+  exClass,
+  color,
+  action,
+}) => {
+  return (
+    <button className={`base-button ${color} ${exClass}`} onClick={action()}>
+      {iconName && <span className="material-symbols-rounded">{iconName}</span>}
+      {text}
+    </button>
+  );
+};
+
+export default Button;
