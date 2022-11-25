dcfb14008f9d5bf9896ada4e1a4d1450ed27e19d
Kahraman <80291704+kahramanX@users.noreply.github.com>
2022-11-25 18:53:54 +0300

    edit apiRoutes

--- server/src/routes/apiRoutes.ts
+++ server/src/routes/apiRoutes.ts
@@ -3,13 +3,22 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 // Member Actions
+// Member GETs
 router.get("/all-member", (req: Request, res: Response) => {
-  res.json({ what: "yeaaa" });
+  res.json({ info: "all member" });
+});
+
+// Member POSTs
+router.post("/add", (req: Request, res: Response) => {
+  res.json();
 });
 
 // Group Actions
+// Group GETs
 router.get("/all-group", (req: Request, res: Response) => {
-  res.json({ what: "yeaaa" });
+  res.json({ info: "all group" });
 });
+
+// Group POSTs
 
 export default router;
