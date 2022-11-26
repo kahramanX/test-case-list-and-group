b874c54bee6fef477471a3346eaf5b38b1a36518
Kahraman <80291704+kahramanX@users.noreply.github.com>
2022-11-26 17:21:14 +0300

    addMemberModal bugs fixed

--- client/src/Components/Shared/Modals/AddMemberModal.tsx
+++ client/src/Components/Shared/Modals/AddMemberModal.tsx
@@ -63,131 +63,150 @@ const AddMemberModal: React.FC<Props> = ({
       });
   }
 
-  useEffect(() => {}, []);
-
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={() => {
         cancelBtnAction();
+        reset();
       }}
       style={customStyles}
       contentLabel="View And Edit Modal"
     >
       <div className="viewedit-modal-text-container">
-        <div className="ve-modal-header">
-          <div className="ve-modal-title">Add Member</div>
-          <div className="ve-modal-edit-btn"></div>
-        </div>
-        <div className="ve-modal-table-container">
-          <table>
-            <tbody>
-              <tr className="table-image">
-                <td className="table-title">
-                  <img src={`${getBase64Code}`} alt="upload-an-img" />
-                </td>
-                <td>
-                  <input
-                    type="file"
-                    id="img"
-                    name="img"
-                    accept="image/*"
-                    onChange={(event) => {
-                      readFile(event.target.files);
-                      console.log(event.target);
-                    }}
-                  />
-                  {getBase64Code === undefined && (
-                    <span className="required">*Required</span>
-                  )}
-                </td>
-              </tr>
-              <tr>
-                <td className="table-title">First Name:</td>
-                <td>
-                  <input
-                    type={"text"}
-                    className="modal-input"
-                    {...register("firstName", { required: true })}
-                  />
-                  {errors.firstName && (
-                    <span className="required">*Required</span>
-                  )}
-                </td>
-              </tr>
-              <tr>
-                <td className="table-title">Last Name: </td>
+        <form
+          onSubmit={handleSubmit((data: any) => {
+            console.log({ ...data, imageBase64: getBase64Code });
+            console.log("saved");
+            getMembersDataFromAPI();
+            //postMemberInfoToApi();
+            cancelBtnAction();
+            reset();
+          })}
+        >
+          <div className="ve-modal-header">
+            <div className="ve-modal-title">Add Member</div>
+            <div className="ve-modal-edit-btn"></div>
+          </div>
+          <div className="ve-modal-table-container">
+            <table>
+              <tbody>
+                <tr className="table-image">
+                  <td className="table-title">
+                    {getBase64Code && (
+                      <img src={`${getBase64Code}`} alt="upload-an-img" />
+                    )}
+                  </td>
+                  <td>
+                    <input
+                      type="file"
+                      id="img"
+                      // name="img"
+                      accept="image/*"
+                      {...register("imageBase64", { required: true })}
+                      onChange={(event) => {
+                        readFile(event.target.files);
+                        console.log(event.target);
+                      }}
+                    />
+                    {errors.imageBase64 && (
+                      <span className="required">*Required</span>
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="table-title">First Name:</td>
+                  <td>
+                    <input
+                      type={"text"}
+                      className="modal-input"
+                      {...register("firstName", { required: true })}
+                    />
+                    {errors.firstName && (
+                      <span className="required">*Required</span>
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="table-title">Last Name: </td>
 
-                <td>
-                  <input
-                    type={"text"}
-                    className="modal-input"
-                    {...register("lastName", { required: true })}
-                  />
-                  {errors.lastName && errors.name.type === "required" && (
-                    <span className="required">*Required</span>
-                  )}
-                </td>
-              </tr>
-              <tr>
-                <td className="table-title">E-mail:</td>
+                  <td>
+                    <input
+                      type={"text"}
+                      className="modal-input"
+                      {...register("lastName", { required: true })}
+                    />
+                    {errors.lastName && (
+                      <span className="required">*Required</span>
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="table-title">E-mail:</td>
 
-                <td>
-                  <input
-                    type={"email"}
-                    className="modal-input"
-                    {...register("email", { required: true })}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="table-title">Phone:</td>
+                  <td>
+                    <input
+                      type={"email"}
+                      className="modal-input"
+                      {...register("email", { required: true })}
+                    />
+                    {errors.email && (
+                      <span className="required">*Required</span>
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="table-title">Phone:</td>
 
-                <td>
-                  <input
-                    type={"phone"}
-                    className="modal-input"
-                    {...register("phone", { required: true })}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="table-title">Birthday:</td>
+                  <td>
+                    <input
+                      type={"phone"}
+                      className="modal-input"
+                      {...register("phone", { required: true })}
+                    />
+                    {errors.phone && (
+                      <span className="required">*Required</span>
+                    )}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="table-title">Birthday:</td>
 
-                <td>
-                  <input
-                    type={"date"}
-                    className="modal-input"
-                    {...register("birthday", { required: true })}
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="ve-modal-buttons">
-          <Button
-            text={cancelBtnText}
-            size={"md"}
-            color={"red-border"}
-            action={() => cancelBtnAction()}
-          />
-          <Button
-            text={confirmBtnText}
-            size={"md"}
-            color={"blue"}
-            action={() => {
-              console.log("saved");
-              getMembersDataFromAPI();
-              cancelBtnAction();
-              handleSubmit((data: any) => {
-                //postMemberInfoToApi();
-                console.log(data);
-                //reset({});
-              });
-            }}
-          />
-        </div>
+                  <td>
+                    <input
+                      type={"date"}
+                      className="modal-input"
+                      {...register("birthday", { required: true })}
+                    />
+                    {errors.birthday && (
+                      <span className="required">*Required</span>
+                    )}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="ve-modal-buttons">
+            <Button
+              text={cancelBtnText}
+              size={"md"}
+              color={"red-border"}
+              action={() => {
+                cancelBtnAction();
+                reset();
+              }}
+            />
+            <Button
+              btnType={"submit"}
+              text={confirmBtnText}
+              size={"md"}
+              color={"blue"}
+              action={() => {
+                getMembersDataFromAPI();
+              }}
+            />
+          </div>
+        </form>
       </div>
     </Modal>
   );
--- server/src/controllers/memberListController.ts
+++ server/src/controllers/memberListController.ts
@@ -39,19 +39,19 @@ export const addMemberController = (req: Request, res: Response) => {
   addMember.save((err: any, result: any) => {
     if (result) {
       console.log("Member Added ✅ ");
+      res.json({ status: true });
 
-      //const addToMemberList = MemberListModel({});
-
-      MemberListModel.findOne({ _id: result._id })
+      /* MemberListModel.findOne({ _id: result._id })
         .populate("MemberList")
         .then(function (memberList: any) {
           //          if (err1) return err1;
           console.log("The xxxx is ", memberList);
           // prints "The author is Ian Fleming"
-        });
+        }); */
     } else {
       console.log("Member Could Not Added ❌ ");
       console.log(err);
+      res.json({ status: false });
     }
   });
 };
